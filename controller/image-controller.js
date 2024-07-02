1aeb1cc7d2861a8477f5c4e05d535a753f86dd2e
Sohailcode <100mohdsohail@gmail.com>
2024-07-02 13:46:39 +0530

    Image api upadted

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -11,7 +11,7 @@ export const uploadImage = async (request, response) => {
     
     try {
         const file = await File.create(fileObj);
-        response.status(200).json({ path: `https://file-share-upload.onrender.com${process.env.PORT}/file/${file._id}`});
+        response.status(200).json({ path: `https://file-share-upload.onrender.com/file/${file._id}`});
     } catch (error) {
         console.error(error.message);
         response.status(500).json({ error: error.message });
